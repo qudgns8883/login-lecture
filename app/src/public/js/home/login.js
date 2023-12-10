e0c1079666f0fda01872355dd73e6ce6934305c6
unknown <>
2023-12-10 18:39:29 +0900

    돔을 이용하여 html 객체저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict"
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+  console.log(id);
+  console.log("bye");
+
+  loginBtn.addEventListener("click", login);
+
+   function login() {
+   const req = {
+     id: id.value,
+    psword: psword.value,
+   };
+   console.log(req);
+   }
