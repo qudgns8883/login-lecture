ead50e80987af5a799698dad4759e1e560389bb9
unknown <>
2023-12-19 22:48:06 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -22,11 +22,15 @@ constructor(body) {
         }
     return { success: false, msg: "존재하지 않는 아이디입니다."};
     }
- register() {
-        const client = this.body;
-        const response = UserStorage.save(client);
-        return response
+ async register() {
+    const client = this.body;
+    try{
+       const response = await UserStorage.save(client);
+       return response
+    } catch(err) {
+        return { success: false, msg: err };
     }
+  }
 }
 
 
